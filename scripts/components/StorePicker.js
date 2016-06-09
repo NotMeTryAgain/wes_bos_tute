6d89c23ca172734bcc383fbb1b7cefc98cd50ae1
MichaelHebert <evogashira@gmail.com>
2016-06-09 19:20:48 -0400

    converted to ES6

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event){
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event) {
     event.preventDefault();
     //get data from input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-    //transition from storepicker to App
+  }
 
-  },
-  render : function() {
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -21,6 +23,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
